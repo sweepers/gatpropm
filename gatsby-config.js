b53175886df6774126e9cd4cc0572a49ae14b180
gppromp <deoz@windowslive.com>
2019-04-15 14:33:55 +0700

    tran-jspdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
-    'jspdf',
+    `gatsby-transformer-pdf`,
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
