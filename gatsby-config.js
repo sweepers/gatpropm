90aef0549a37c26906008a9b472b61bcbbc5dd40
gppromp <deoz@windowslive.com>
2019-04-15 13:21:20 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'บริการการยื่นคำร้อง',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'บริการการยื่นคำร้องขอเปิดบริษัท',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
