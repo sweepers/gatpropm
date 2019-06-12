9140a6f96cae3f96726f67f483ea2d03020ef5ec
gppromp <deoz@windowslive.com>
2019-06-12 23:51:37 +0700

    page_about

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,7 +3,17 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-
+import faature_1 from '../img/features/1.png'
+import faature_2 from '../img/features/2.png'
+import faature_3 from '../img/features/3.png'
+import faature_4 from '../img/features/4.png'
+import img_abt from '../img/about-author.png'
+import img_service1 from '../img/service/service_1.jpg'
+import img_service2 from '../img/service/service_2.jpg'
+import img_service3 from '../img/service/service_3.jpg'
+import img_service4 from '../img/service/service_4.jpg'
+import img_service5 from '../img/service/service_5.jpg'
+import img_service6 from '../img/service/service_6.jpg'
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
@@ -22,6 +32,98 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
             </div>
         </div>
     </section>
+    <section class="features_area" id="features_counter">
+      <div class="container">
+        <div class="row counter_wrapper">
+        
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single_feature">
+              <div class="thumb">
+                <img src={faature_1} alt="" />
+              </div>
+              <div class="info-content">
+                <h4><span class="counter">596</span>+</h4>
+                <p>Qualified Lawyer</p>
+
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single_feature">
+              <div class="thumb">
+                <img src={faature_2} alt="" />
+              </div>
+              <div class="info-content">
+                <h4><span class="counter">20650</span>+</h4>
+                <p>Solved Cases</p>
+              </div>
+            </div>
+          </div>
+         
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single_feature">
+              <div class="thumb">
+                <img src={faature_3} alt="" />
+              </div>
+              <div class="info-content">
+                <h4><span class="counter">2.5</span>k</h4>
+                <p>Trusted Clients</p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single_feature">
+              <div class="thumb">
+                <img src={faature_4} alt="" />
+              </div>
+              <div class="info-content">
+                <h4><span class="counter">50</span>+</h4>
+                <p>Achievements</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="video-sec-area section_gap_top" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 video-left overlay">
+            <div class="video-inner justify-content-center align-items-center d-flex">
+              <a id="play-home-video" class="video-play-button" href="">
+                <span></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-6 video-right">
+            <h1>Get to Know Project Estimate?</h1>
+            <p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+              exciting to think about setting up your own viewing station whether that is on the deck.</p>
+            <a href="#" class="primary-btn">Learn More</a>
+
+            <div class="right-bottom-area">
+              <div class="author-lacture">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt labore dolore magna
+                  aliqua enim minim veniam quis nostrud. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod
+                  tempor incididunt labore dolore magna aliqua enim minim veniam quis nostrud. Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit, sed eiusmod tempor incididunt labore dolore magna aliqua enim minim veniam quis
+                  nostrud.</p>
+                <div class="author-title">
+                  <div class="thumb"><img src={img_abt} alt="" /></div>
+                  <div class="a-desc">
+                    <h6>Marvel Maison</h6>
+                    <p>Chief Executive, Amazon</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="about-area"></section>
     <section className="section section--gradient">
       <div className="container">
         <div className="columns">
