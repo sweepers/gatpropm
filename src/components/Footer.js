94f362b2ebfa1cae54bfd99a0f0f3e5dc4a0372a
gppromp <deoz@windowslive.com>
2019-06-12 21:37:13 +0700

    update_img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,107 @@ import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
-
+import i1 from '../img/i1.jpg'
+import i2 from '../img/i2.jpg'
+import i3 from '../img/i3.jpg'
+import i4 from '../img/i4.jpg'
+import i5 from '../img/i5.jpg'
+import i6 from '../img/i6.jpg'
+import i7 from '../img/i7.jpg'
+import i8 from '../img/i8.jpg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-        © 2016 Prompt Legal Sevice Co., Ltd. All Rights Reserved.
-        </div>
-       
-      </footer>
+      <footer class="footer-area">
+		<div class="footer_top section_gap_top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<div class="single-footer-widget">
+							<h5 class="footer_title">About Lawful</h5>
+							<p class="about-text">The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point where images and videos are used more to </p>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-6 col-sm-6">
+						<div class="single-footer-widget">
+							<h5 class="footer_title">Navigation Links</h5>
+							<div class="row">
+								<div class="col-5">
+									<ul class="list">
+										<li><a href="#">Home</a></li>
+										<li><a href="#">Features</a></li>
+										<li><a href="#">Services</a></li>
+										<li><a href="#">Portfolio</a></li>
+									</ul>
+								</div>
+								<div class="col-5">
+									<ul class="list">
+										<li><a href="#">Team</a></li>
+										<li><a href="#">Pricing</a></li>
+										<li><a href="#">Blog</a></li>
+										<li><a href="#">contact</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6">
+						<div class="single-footer-widget">
+							<h5>Newsletter</h5>
+							<p>For business professionals caught between high OEM price and mediocre print and graphic output, </p>
+							<div class="footer-newsletter" id="mc_embed_signup">	
+								
+									<div class="d-flex flex-row">
+										<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
+										 required="" type="email" />
+										<button class="click-btn btn btn-default"><span class="lnr lnr-location" aria-hidden="true"></span></button>
+										<div style="position: absolute; left: -5000px;">
+											<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text" />
+										</div>
+									</div>
+									<div class="info"></div>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3  col-md-6 col-sm-6">
+						<div class="single-footer-widget mail-chimp">
+							<h5 class="mb-20">Instragram Feed</h5>
+							<ul class="instafeed d-flex flex-wrap">
+								<li><img src="{i1}" alt="" /></li>
+								<li><img src="{i2}" alt="" /></li>
+								<li><img src="{i3}" alt="" /></li>
+								<li><img src="{i4}" alt="" /></li>
+								<li><img src="{i5}" alt="" /></li>
+								<li><img src="{i6}" alt="" /></li>
+								<li><img src="{i7}" alt="" /></li>
+								<li><img src="{i8}" alt="" /></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-12">
+						<p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+</p>
+					</div>
+					<div class="col-lg-6 col-md-12 text-right">
+						<div class="social-icons">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
     )
   }
 }
