d246f102310606a9d540c542ae9b60781c959faf
gppromp <deoz@windowslive.com>
2019-08-11 16:00:29 +0700

    requier firebase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = class extends React.Component {
 		uid:'MzmCVg9pCnYPslxJrfN8cQs4uJ42',
 	  };
 	  console.log('config',config);
-	  firebase.initializeApp(config);
+	  //firebase.initializeApp(config);
 	  this.state = {
 		current_user: null,
 	   
