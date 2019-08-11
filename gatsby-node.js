ea5279fd1d36a16208100f6441b7229554c3d79d
gppromp <deoz@windowslive.com>
2019-08-11 17:32:57 +0700

    ag

--- gatsby-node.js
+++ gatsby-node.js
@@ -71,16 +71,6 @@ exports.createPages = ({ actions, graphql }) => {
     })
   })
 }
-exports.modifyWebpackConfig = ({ config, stage }) => {
-  switch (stage) {
-    case "build-html":
-      config.plugin('define', webpack.DefinePlugin, [ { "global.GENTLY": false } ]);
-
-        break;
-  }
-
-  return config;
-};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
