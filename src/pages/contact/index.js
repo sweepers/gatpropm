ece1eac76ab006788ac0e43f3406c434dc07793a
gppromp <deoz@windowslive.com>
2019-08-11 22:03:32 +0700

    s

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -38,6 +38,7 @@ export default class Index extends React.Component {
       <Layout>
         <section class="contact_area section_gap">
 		<div class="container">
+            
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.870803963179!2d100.5078619152053!3d13.726270790364028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e299cd81f01815%3A0x1660bd86761a92f4!2z4LmE4Lit4LiE4Lit4LiZ4Liq4Lii4Liy4LihIChJQ09OU0lBTSk!5e0!3m2!1sth!2sth!4v1565534838828!5m2!1sth!2sth" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 			<div class="row">
 				<div class="col-lg-3">
