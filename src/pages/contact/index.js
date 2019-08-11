af89d3d49fcc99fde50bd48126854cc64017593a
gppromp <deoz@windowslive.com>
2019-08-11 22:14:08 +0700

    hmml

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
-
+import Helmet from 'react-helmet'
 function encode(data) {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
