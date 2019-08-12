3b11e30e962ff6197d5121391f364d8564e1a756
gppromp <deoz@windowslive.com>
2019-08-12 16:40:04 +0700

    localstorage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,10 @@ const Navbar = class extends React.Component {
 		current_user: null
 	  
 	};
-	//const current_user = localStorage.getItem('current_user');
+	if (!window) {
+		require('localstorage-polyfill');
+	}
+	let current_user = localStorage.getItem('current_user');
 	//console.log('current_user',current_user);
 	//let current_user  = JSON.parse(localStorage.getItem('current_user'));
 	//console.log('current_user',current_user);
