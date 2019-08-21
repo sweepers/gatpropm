c13345e2b8e143f4eb12348a1cca02426e88efb3
gppromp <deoz@windowslive.com>
2019-08-21 21:32:26 +0700

    cp

--- src/pages/application_form/dashboard.js
+++ src/pages/application_form/dashboard.js
@@ -22,7 +22,9 @@ function encode(data) {
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props)
-    
+    if(!this.state){
+      this.state = {}
+    }
    
     //this.setState({'password':'123456'});
    /* this.state = {
@@ -30,7 +32,50 @@ export default class Dashboard extends React.Component {
       avatarURL: "",
       avatar: "",
     };*/
-    
+    if (!firebase.apps.length) {
+      var config = {
+        apiKey: "AIzaSyAcuRid40eaVEx5TKKn5yibuDTFNDyQxAU",
+        authDomain: "promp-d2fc3.firebaseapp.com",
+        databaseURL: "https://promp-d2fc3.firebaseio.com",
+        projectId: "promp-d2fc3",
+        storageBucket: "promp-d2fc3.appspot.com",
+        messagingSenderId: "660976897097",
+        uid:'MzmCVg9pCnYPslxJrfN8cQs4uJ42',
+        };
+      firebase.initializeApp(config);
+     }
+    this.handleChange_date = this.handleChange_date.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    let current_user  = JSON.parse(localStorage.getItem('current_user'));
+    console.log('current_user',current_user)
+    var  data = {};
+    if(current_user != null){
+           
+      let app = firebase.database().ref('data_company/'+current_user.id+'/');
+     
+      app.on('value', snapshot => {
+          let data = snapshot.val();
+         // let date = data.company_date.split('/');
+         // let company_date = new Date(date[2],date[1]-1,date[0]);
+          delete data.company_date;
+          delete data.updated;
+          
+          this.setState(data);
+          this.setState({
+            user_id:current_user.id
+        
+          });
+          console.log('state',this.state);
+          
+         
+
+        
+            
+            
+      }).bind(this);
+      
+      
+  }
     
     
     
@@ -98,54 +143,11 @@ export default class Dashboard extends React.Component {
   }
 
   render() {
-    if (!firebase.apps.length) {
-      var config = {
-        apiKey: "AIzaSyAcuRid40eaVEx5TKKn5yibuDTFNDyQxAU",
-        authDomain: "promp-d2fc3.firebaseapp.com",
-        databaseURL: "https://promp-d2fc3.firebaseio.com",
-        projectId: "promp-d2fc3",
-        storageBucket: "promp-d2fc3.appspot.com",
-        messagingSenderId: "660976897097",
-        uid:'MzmCVg9pCnYPslxJrfN8cQs4uJ42',
-        };
-      firebase.initializeApp(config);
-     }
-    this.handleChange_date = this.handleChange_date.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    let current_user  = JSON.parse(localStorage.getItem('current_user'));
-    console.log('current_user',current_user)
-    var  data = {};
-    if(current_user != null){
-           
-      let app = firebase.database().ref('data_company/'+current_user.id+'/');
-     
-      app.on('value', snapshot => {
-          let data = snapshot.val();
-         // let date = data.company_date.split('/');
-         // let company_date = new Date(date[2],date[1]-1,date[0]);
-          delete data.company_date;
-          delete data.updated;
-          
-          this.setState(data);
-          this.setState({
-            user_id:current_user.id
-        
-          });
-          console.log('state',this.state);
-          
-         
-
-        
-            
-            
-      }).bind(this);
-      
-      
-  }
+    
     var avatar = '';
-       /* if(this.state.avatarURL){
+        if(this.state.avatarURL){
               avatar = <div className="logo_img"><img src={this.state.avatarURL} /> <a onClick={() => this.onRemove('')}  >Remove</a></div>;
-        }*/
+        }
     return (
       <Layout>
         <section className="section">
